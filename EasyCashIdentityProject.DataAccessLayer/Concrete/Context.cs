d99241c2439b30880e08a4c25458a3e562d0b249
isnuraina <nurane.ismayilzade19@gmail.com>
2026-02-01 19:16:58 +0400

    #4 Easy Cash Asp.Net Core 6.0 Identity Projesi - Identity Kütüphanesinin Eklenmesi

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,15 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-MPV3150\\SQLEXPRESS;initial catalog=EasyCashDb;integrated Security=true;");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
